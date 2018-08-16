a6cba180e9fdd2cd5f577a1a6bf002de4f4e1fac
Olegok <NZOlegok@gmail.com>
2018-08-16 20:01:31 +1200

    basic UI and video representation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-const App = () => {
-	return <div>Hi!</div>;
-};
+import SearchBar from './components/search_bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyCGBSJv-u4rApDZRn3NAHLUUzeMgixDySE';
+
+
+
+class App extends Component {
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			videos: []
+		}
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+			this.setState({videos: data});
+		});
+	}
+
+	render(){
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]}/>
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
